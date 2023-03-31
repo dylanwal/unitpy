59a5673826d2f57c5374f56c1d7d5d589174907d
Dylan <dylanwal@mit.edu>
2023-03-31 17:18:50 -0400

    update setup

--- src/unitpy/unit.py
+++ src/unitpy/unit.py
@@ -25,8 +25,9 @@ class Unit:
     def __repr__(self):
         return self.__str__()
 
-        def __hash__(self) -> int:
+    def __hash__(self) -> int:
         return self._units.__hash__()
+
     def __copy__(self) -> PlainUnit:
         ret = self.__class__(self._units)
         return ret
@@ -35,7 +36,28 @@ class Unit:
         ret = self.__class__(copy.deepcopy(self._units, memo))
         return ret
 
-     def __eq__(self, other) -> bool:
+    def __mul__(self, other):
+        if isinstance(other, Unit):
+            return Unit(f"{self.name}*{other.name}", self.factor * other.factor)
+        else:
+            return Unit(f"{self.name}*{other}", self.factor * other)
+
+    def __rmul__(self, other):
+        return self.__mul__(other)
+
+    def __truediv__(self, other):
+        if isinstance(other, Unit):
+            return Unit(f"{self.name}/{other.name}", self.factor / other.factor)
+        else:
+            return Unit(f"{self.name}/{other}", self.factor / other)
+
+    def __rtruediv__(self, other):
+        return Unit(f"{other}/{self.name}", other / self.factor)
+
+    def __repr__(self):
+        return f"Unit({self.name}, {self.factor})"
+
+    def __eq__(self, other) -> bool:
 
     def __lt__(self, other):
 
@@ -74,58 +96,75 @@ class Unit:
             return False
         return True
 
+    @property
+    def base_unit(self) -> Unit:
+        return self._base_unit
 
 
 class Quantity:
 
-    def __init__(self, value: str | int | float, unit: Unit = None):
+    def __init__(self, value: str | int | float, unit: Unit | str = None):
         if isinstance(value, str):
-            parse(value)
+            value, unit = parse(value)
+        if isinstance(unit, str):
+            unit = Unit(unit)
         self._unit = unit
-        self._value = value
-        self._multipler = multipler
+        self._base_value = value
+        self._multiplier = unit.multiplier
 
     @property
-    def v(self):
-        return self._value
+    def v(self) -> int | float:
+        return self._base_value * self._multiplier
 
     @property
-    def value(self):
-        return self._value
+    def value(self) -> int | float:
+        return self._base_value * self._multiplier
 
     @property
-    def u(self):
+    def u(self) -> Unit:
         return self._unit
 
     @property
-    def unit(self):
+    def unit(self) -> Unit:
         return self._unit
 
     @property
     def base_value(self) -> int | float:
-        return self._value
+        return self._base_value
 
     @property
     def base_unit(self) -> Unit:
-        return self._unit
+        return self._unit.base_unit
+
+    @property
+    def dimensionality(self) -> Dimension:
+        return self.unit.dimensionality
+
+    @property
+    def dim(self) -> Dimension:
+        return self.unit.dimensionality
+
+    @property
+    def dimensionless(self) -> bool:
+        return self.unit.dimensionless
 
     def __str__(self):
-        return str(self._value) + " " + str(self._unit)
+        return f"{self.value} {self.unit}"
 
     def __repr__(self):
-        return self.__str__()
+        return f"Quantity({self.value}, '{self.unit}')"
 
     def __hash__(self) -> int:
         if self._unit.dimensionless:
             return hash(self.value)
         else:
-            return hash((self._value, self._unit))
+            return hash((self._base_value, self._unit))
 
     def __copy__(self) -> Quantity:
-        return self.__class__(copy.copy(self._value), self._unit)
+        return self.__class__(copy.copy(self._base_value), self._unit)
 
     def __deepcopy__(self, memo) -> Quantity:
-        return self.__class__(copy.deepcopy(self._value), self._unit)
+        return self.__class__(copy.deepcopy(self._base_value), self._unit)
 
     def __eq__(self, other) -> bool:
         if not isinstance(other, Quantity):
@@ -173,57 +212,76 @@ class Quantity:
         return self.base_value >= other.base_value
 
     def __iadd__(self, other):
-        if not isinstance(other, Quantity):
-            raise errors.QuantityError("Comparision can't happen")
-        if self.base_unit is not other.base_unit:
-            raise errors.QuantityError("Comparision must have same units")
-
-        self._value += self._value + other._value
+        if isinstance(other, Quantity) and self.unit == other.unit:
+            self._base_value += other._base_value
+            return self
+        else:
+            raise TypeError("Cannot add quantities with different units")
 
     def __add__(self, other):
-        if not isinstance(other, Quantity):
-            raise errors.QuantityError("Comparision can't happen")
-        if self.base_unit is not other.base_unit:
-            raise errors.QuantityError("Comparision must have same units")
-
-        return self.__class__(self._value + self._value, self._unit)
+        if isinstance(other, Quantity) and self.unit == other.unit:
+            return Quantity(self.value + other.value, self.unit)
+        else:
+            raise TypeError("Cannot add quantities with different units")
 
     __radd__ = __add__
 
     def __isub__(self, other):
-        if is_duck_array_type(type(self._magnitude)):
-            return self._iadd_sub(other, operator.isub)
+        if isinstance(other, Quantity) and self.unit == other.unit:
+            self._base_value -= other._base_value
+            return self
         else:
-            return self._add_sub(other, operator.sub)
+            raise TypeError("Cannot subtract quantities with different units")
 
     def __sub__(self, other):
-        return self._add_sub(other, operator.sub)
+        if isinstance(other, Quantity) and self.unit == other.unit:
+            return Quantity(self.value - other.value, self.unit)
+        else:
+            raise TypeError("Cannot subtract quantities with different units")
 
     def __rsub__(self, other):
-        if isinstance(other, datetime.datetime):
-            return other - self.to_timedelta()
+        if isinstance(other, Quantity) and self.unit == other.unit:
+            return Quantity(other.value - self.value, self.unit)
         else:
-            return -self._add_sub(other, operator.sub)
-
-    def __sub__(self, other):
-        raise errors.UnitError("no subtraction")
+            raise TypeError("Cannot subtract quantities with different units")
 
     def __imul__(self, other):
+        if isinstance(other, (int, float)):
+            self._base_value *= other
+            return self
+        elif isinstance(other, Quantity):
+            self._base_value *= other.value
+            self._unit *= other.unit
+            return self
+        else:
+            raise TypeError("Can only multiply Quantity by scalar")
+
     def __mul__(self, other):
-        if isinstance(other, Unit):
-            return Unit()
-        elif isinstance(other, int) or isinstance(other, float):
-            return self.value * other
+        if isinstance(other, (int, float)):
+            return Quantity(self.value * other, self.unit)
+        elif isinstance(other, Quantity):
+            return Quantity(self.value * other.value, self.unit * other.unit)
+        else:
+            raise TypeError("Can only multiply Quantity by scalar")
+
      __rmul__ = __mul__
 
     def __truediv__(self, other):
-        return self.value / other
+        if isinstance(other, (int, float)):
+            return Quantity(self.value / other, self.unit)
+        else:
+            raise TypeError("Can only divide Quantity by scalar")
+
+    def __itruediv__(self, other):
+        if isinstance(other, (int, float)):
+            self.value /= other
+            return self
+        else:
+            raise TypeError("Can only divide Quantity by scalar")
 
     def __pow__(self, power, modulo=None):
         if isinstance(power, int) or isinstance(power, float):
             return Unit() self.value**power
-
-
 
     def __int__(self) -> int:
         return Quantity(int())
@@ -233,83 +291,6 @@ class Quantity:
 
     def __floor__(self):
         return Quantity
-def __mod__(self, other):
-    # def __copy__(self) -> Unit:
-    #     ret = self.__class__(self._units)
-    #     ret.__used = self.__used
-    #     return ret
-    #
-    # def __deepcopy__(self, memo) -> Unit:
-    #     ret = self.__class__(copy.deepcopy(self._units, memo))
-    #     ret.__used = self.__used
-    #     return ret
-    #
-    # def __str__(self) -> str:
-    #     return format(self)
-    #
-    # def __bytes__(self) -> bytes:
-    #     return str(self).encode(locale.getpreferredencoding())
-    #
-    # def __repr__(self) -> str:
-    #     return "<Unit('{}')>".format(self._units)
-    #
-    # def __format__(self, spec) -> str:
-    #     spec = spec or self.default_format
-    #     # special cases
-    #     if "Lx" in spec:  # the LaTeX siunitx code
-    #         return r"\si[]{%s}" % siunitx_format_unit(self)
-    #
-    #     if "~" in spec:
-    #         if not self._units:
-    #             return ""
-    #         units = UnitsContainer(
-    #             dict(
-    #                 (self._REGISTRY._get_symbol(key), value)
-    #                 for key, value in self._units.items()
-    #             )
-    #         )
-    #         spec = spec.replace("~", "")
-    #     else:
-    #         units = self._units
-    #
-    #     return format(units, spec)
-    #
-    # def __mul__(self, other):
-    #     if self._check(other):
-    #         if isinstance(other, self.__class__):
-    #             return self.__class__(self._units * other._units)
-    #         else:
-    #             qself = self._REGISTRY.Quantity(1, self._units)
-    #             return qself * other
-    #
-    #     if isinstance(other, Number) and other == 1:
-    #         return self._REGISTRY.Quantity(other, self._units)
-    #
-    #     return self._REGISTRY.Quantity(1, self._units) * other
-    #
-    # def __truediv__(self, other):
-    #     if self._check(other):
-    #         if isinstance(other, self.__class__):
-    #             return self.__class__(self._units / other._units)
-    #         else:
-    #             qself = 1 * self
-    #             return qself / other
-    #
-    #     return self._REGISTRY.Quantity(1 / other, self._units)
-    #
-    # @property
-    # def dimensionality(self):
-    #     """
-    #     Returns
-    #     -------
-    #     dict
-    #         Dimensionality of the Unit, e.g. ``{length: 1, time: -1}``
-    #     """
-    #     return self._dimensionality
 
-    # def parse(self, input: str):
-    #     """ Pares string into Units."""
-    #     seperate = self._parse_muliple_units()
-    #     prefix, base = self._parse_prefix()
-    #
-    # def _parse_muliple_units(self, input: str): list
+    def __mod__(self, other):
+
